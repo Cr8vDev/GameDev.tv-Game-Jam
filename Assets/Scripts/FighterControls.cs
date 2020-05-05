0aab975d5c241ef3faaf356197cfe7a08bae3a02
Cr8v Dev <cr8vdev@gmail.com>
2020-05-05 17:18:27 +0200

    Movement System

---
+++ Assets/Scripts/FighterControls.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterControls : MonoBehaviour
+{
+    [SerializeField]
+    private float rotationSpeed;
+    [SerializeField]
+    private float speed;
+
+    private Transform fighterChild;
+
+    float rotationZ;
+
+    float t = 0f;
+
+    private void Start()
+    {
+        fighterChild = transform.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+        transform.position += transform.forward * speed;
+
+        if (!Mathf.Approximately(x, 0f) || !Mathf.Approximately(y, 0f))
+        {
+            if (x < 0f)
+            {
+                var lerpRotation = Mathf.LerpAngle(fighterChild.localEulerAngles.z, 30f, Time.deltaTime);
+
+                fighterChild.localEulerAngles = new Vector3(0f, fighterChild.localEulerAngles.y, lerpRotation);
+
+                transform.eulerAngles -= new Vector3(0f, Time.deltaTime * rotationSpeed, 0f);
+            }
+            else if(x > 0f)
+            {
+                var lerpRotation = Mathf.LerpAngle(fighterChild.localEulerAngles.z, -30f, Time.deltaTime);
+
+                fighterChild.localEulerAngles = new Vector3(0f, fighterChild.localEulerAngles.y, lerpRotation);
+
+                transform.eulerAngles += new Vector3(0f, Time.deltaTime * rotationSpeed, 0f);
+            }
+
+            transform.eulerAngles -= new Vector3(y * Time.deltaTime * rotationSpeed, 0.0f, 0f);
+        }
+
+        if (Mathf.Approximately(x, 0f))
+        {
+            var lerpRotation = Mathf.LerpAngle(fighterChild.localEulerAngles.z, 0f, Time.deltaTime);
+
+            fighterChild.localEulerAngles = new Vector3(0f, fighterChild.localEulerAngles.y, lerpRotation);
+        }
+    }
+}
---
+++ Assets/URP Config Files/Scripts/Zombie.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Zombie : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    private Vector3 targetPosition;
+
+    [SerializeField]
+    float inputDelay;
+    float pathDelay;
+
+    NavMeshHit hit;
+    NavMeshPath path;
+
+    NavMeshAgent agent;
+    NavMeshObstacle obstacle;
+
+    private void Start()
+    {
+        path = new NavMeshPath();
+
+        pathDelay = inputDelay;
+
+        agent = GetComponent<NavMeshAgent>();
+        obstacle = GetComponent<NavMeshObstacle>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.realtimeSinceStartup > pathDelay)
+        {
+            NavMesh.CalculatePath(transform.position, target.position, NavMesh.AllAreas, path);
+
+            pathDelay = Time.realtimeSinceStartup + inputDelay;
+        }
+
+        if (agent.isActiveAndEnabled)
+        {
+            if (path.status == NavMeshPathStatus.PathPartial)
+            {
+                targetPosition = path.corners[path.corners.Length - 1];
+            }
+            else
+            {
+                targetPosition = target.position;
+            }
+
+            if (Vector3.Distance(transform.position, targetPosition) < 2.0f)
+            {
+                agent.enabled = false;
+                obstacle.enabled = true;
+            }
+            else
+            {
+                if (agent.isActiveAndEnabled)
+                {
+                    agent.destination = targetPosition;
+                }
+            }
+        }
+    }
+}
